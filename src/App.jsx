ea675f16bcc6306b1de4389df816234cd871c7c5
Joao Fuzer <fuuzer_@hotmail.com>
2025-10-19 18:50:06 -0300

    changed images and fixed about

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,24 @@ function App() {
     <>
       <Header />
       <main className='container'>
-        <img src="./gabrielle.png" alt="" />
         <div className='about-me'>
-          <h1>SOBRE MIM</h1>
-          <p>Sou fotógrafa de Esteio, no Rio Grande do Sul. Trabalho com fotografia de eventos, ensaios e retratos, sempre buscando registrar momentos com leveza, afeto e autenticidade. Acredito que cada imagem carrega uma história e o meu olhar é voltado para transformar essas histórias em memórias cheias de significado :)</p>
+          <img src="./gabrielle.png" alt="" />
+          <div>
+            <h1>SOBRE MIM</h1>
+            <p>Sou fotógrafa de Esteio, no Rio Grande do Sul. Trabalho com fotografia de eventos, ensaios e retratos, sempre buscando registrar momentos com leveza, afeto e autenticidade. Acredito que cada imagem carrega uma história e o meu olhar é voltado para transformar essas histórias em memórias cheias de significado :)</p>
+          </div>
         </div>
       </main>
 
       <section className='hero-section'>
         <div className='images'>
-          <img src="./foto-4.png" alt="" />
+          <img src="./fotoElton.png" alt="" />
+          <img src="./Frame-1.png" alt="" />
+          <img src="./Foto-2.png" alt="" />
         </div>
       </section>
+
+      <section></section>
     </>
   )
 }
